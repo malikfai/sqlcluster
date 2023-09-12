c86a63ecf8ac55021744b8dfd726e2eb726f46bb
Ben Hlaban <behlaban@microsoft.com>
2023-09-11 20:34:18 -0500

    Fixed witnessStorageEndpoint

--- sqlfci/deploy-helper.ps1
+++ sqlfci/deploy-helper.ps1
@@ -2,14 +2,14 @@ Connect-AzAccount -Environmentname AzureUSGovernment
 
 $deploymentName = "deploy-sqlcluster-fci"
 $resourceGroupName = "sqlcluster"
-$namePrefix = "fm-fci"
+$namePrefix = "fmfci"
 $existingDomainName = "contoso.local"
 $existingAdminUsername = "fmadmin"
 $existingAdminPassword = "!Welcome2023" #Read-Host -MaskInput -Prompt "Enter domain admin password"
 $existingSqlServiceAccountUserName = "sqlservice"
 $existingSqlServiceAccountPassword = "!Welcome2023" #Read-Host -MaskInput -Prompt "Enter SQL service account password"
 $existingSubnetResourceID = "/subscriptions/22d888ba-fc6d-4539-8483-172985f2a28f/resourceGroups/sqlcluster/providers/Microsoft.Network/virtualNetworks/autohav2VNEThef/subnets/sqlSubnet"
-$witnessType = "Disk"
+$witnessType = "Cloud"
 $_artifactsLocation = "https://raw.githubusercontent.com/malikfai/sqlcluster/main/sqlfci/"
 
 $templateParameterObject = @{
